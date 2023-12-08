02e5dfdc7e4aaebdc0b21d4bc9fcaeecea896958
mellvik <helge@mindset3.org>
2023-12-08 18:36:55 +0100

    [net] Cleanups

--- tlvc/arch/i86/drivers/net/netbuf.c
+++ tlvc/arch/i86/drivers/net/netbuf.c
@@ -10,13 +10,12 @@ struct netbuf *netbuf_init(struct netbuf *buf, int cnt) {
 	int i;
 	for (i = 0; i < cnt; i++) {
 #ifdef USE_HEAP_BUFFER
-		buf[i].data = NULL;
 		if (!(buf[i].data = heap_alloc(MAX_PACKET_ETH, HEAP_TAG_BUFHEAD))) {
 			printk("eth: Buffer alloc failed\n");
 			netbuf_release(&buf[0]);
 			return(NULL);
 		}
-		printk("netbuf got %x\n", buf[i].data);
+		//printk("netbuf got %x\n", buf[i].data);
 #endif
 		buf[i].len = 0;
 		buf[i].next = &buf[i+1];
@@ -30,7 +29,7 @@ void netbuf_release(struct netbuf *buf) {
 	struct netbuf *n = buf;
 	do {
 		heap_free(n->data);
-		printk("netbuf rel %x\n", n->data);
+		//printk("netbuf rel %x\n", n->data);
 		n = n->next;
 	} while (n != buf);
 }
