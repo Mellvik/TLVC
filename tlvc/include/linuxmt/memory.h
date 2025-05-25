7de86c0d98b62c4b9bf0bd83493acf7930826068
mellvik <helge@mindset3.org>
2025-05-25 18:13:24 +0200

    [kernel] Allow floppy DMA to/frm XMS memory, probe fixes

--- tlvc/include/linuxmt/memory.h
+++ tlvc/include/linuxmt/memory.h
@@ -31,6 +31,7 @@ word_t fmemcmpw (void * dst_off, seg_t dst_seg, void * src_off, seg_t src_seg, s
 #define _FP_OFF(fp)     ((unsigned)(unsigned long)(void __far *)(fp))
 #define _MK_FP(seg,off)	((void __far *)((((unsigned long)(seg)) << 16) | (off)))
 #define _MK_LINADDR(seg, offs) ((unsigned long)((((unsigned long)(seg)) << 4) + (unsigned)(offs)))
+#define _MK_XMSADDR(seg, offs) ((unsigned long)(((unsigned long)(seg)) + (unsigned)(offs)))
 
 /* unreal mode, A20 gate management */
 int check_unreal_mode(void);	/* check if unreal mode capable, returns > 0 on success */
