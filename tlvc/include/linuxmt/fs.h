41dada787475b541cc471d98df25a53ac16f6857
mellvik <helge@mindset3.org>
2025-06-09 10:59:46 +0200

    [kernel] Make register_chrdev and register_blkdev INITPROC

--- tlvc/include/linuxmt/fs.h
+++ tlvc/include/linuxmt/fs.h
@@ -405,16 +405,9 @@ extern struct inode *__iget(struct super_block *,ino_t);
 /*@+namechecks@*/
 
 extern struct file_operations *get_blkfops(unsigned int);
-extern int register_blkdev(unsigned int,const char *,struct file_operations *);
-extern int unregister_blkdev(void);
-extern int blkdev_open(struct inode *,struct file *);
 
 extern struct file_operations def_blk_fops;
 extern struct inode_operations blkdev_inode_operations;
-
-extern int register_chrdev(unsigned int,const char *,struct file_operations *);
-extern int unregister_chrdev(void);
-extern int chrdev_open(struct inode *,struct file *); 
 
 extern struct file_operations def_chr_fops;
 extern struct inode_operations chrdev_inode_operations;
